daaf0c2167e51c8c1fc260271383c5761a6477e2
asyafEvinced <asya.frumkin@evinced.com>
2021-12-29 17:28:57 +0200

    Added fp16 model

--- onnx/convert_to_onnx.py
+++ onnx/convert_to_onnx.py
@@ -1,67 +1,97 @@
 import numpy as np
+import logging
 import onnx
+from onnxconverter_common import convert_float_to_float16_model_path
 import onnxruntime
 from onnxruntime.quantization import quantize_dynamic, QuantType
 from pathlib import Path
+from PIL import Image
+import sys
 import torch
+from torchvision import transforms
 
 from craft_text_detector import Craft
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 ONNX_EXT = '.onnx'
 ONNX_MODEL_NAME = 'craft'
 ONNX_MODEL_FILE = f'{ONNX_MODEL_NAME}{ONNX_EXT}'
+IMG_PATH = '../figures/idcard.png'
 
 
-def convert(model, input_data, output_model_path):
+def test_onnx_model(model_path, input_data):
+    onnx_model = onnx.load(model_path)
+    # onnx.checker.check_model(onnx_model)
+    logging.info(f'Model check of {model_path} passed')
+    run_onnx_model(model_path, input_data)
+    logging.info(f'Model run successfully')
+
+
+def convert_to_onnx(model, input_data, output_model_path):
     torch.onnx.export(model,
                       input_data,
                       str(output_model_path),
                       export_params=True,
-                      opset_version=12,
+                      opset_version=13, # conversion to flp16 doesn't work with lower opset
                       do_constant_folding=True,
                       input_names=['input'],
                       output_names=['output'],
                       dynamic_axes={'input': {0: 'batch_size'},
                                     'output': {0: 'batch_size'}})
-
-    onnx_model = onnx.load(ONNX_MODEL_FILE)
-    onnx.checker.check_model(onnx_model)
+    test_onnx_model(output_model_path, input_data)
 
 
-def test_output(torch_out, input_data, onnx_model):
+def to_numpy(tensor):
+    if tensor.requires_grad:
+        return tensor.detach().cpu().numpy()
+    else:
+        tensor.cpu().numpy()
+
+
+def run_onnx_model(onnx_model, input_data):
     ort_session = onnxruntime.InferenceSession(str(onnx_model))
-
-    def to_numpy(tensor):
-        if tensor.requires_grad:
-            return tensor.detach().cpu().numpy()
-        else:
-            tensor.cpu().numpy()
 
     ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(input_data)}
     ort_outs = ort_session.run(None, ort_inputs)
+    return ort_outs
 
+
+def test_output(torch_out, input_data, onnx_model):
+    ort_outs = run_onnx_model(onnx_model, input_data)
     for i, j in zip(torch_out, ort_outs):
         np.testing.assert_allclose(to_numpy(i), j, rtol=1e-03, atol=1e-05)
 
-    print("Exported model has been tested with ONNXRuntime, and the result looks good!")
+    logging.info("Exported model has been tested with ONNXRuntime, and the result looks good!")
 
 
-def quantize_model_dynamic(onnx_model_path, quantization):
-    model_name = onnx_model_path.name
+def quantize_model_dynamic(onnx_model_path, quantization, input_data):
+    model_name = onnx_model_path.stem
     quantized_model_file = f'{model_name}_quant_{str(quantization)}{ONNX_EXT}'
     quantize_dynamic(onnx_model_path,
                      Path(quantized_model_file),
                      weight_type=quantization)
-
-    print(f"Dynamically quantized model saved to: {quantized_model_file}")
-
-
-def run_quantizations(onnx_model_path):
-    quantize_model_dynamic(onnx_model_path, QuantType.QInt8)
-    quantize_model_dynamic(onnx_model_path, QuantType.QUInt8)
+    test_onnx_model(quantized_model_file, input_data)
+    logging.info(f"Dynamically quantized model saved to: {quantized_model_file}")
 
 
-def main():
+def quantize_model_fp16(onnx_model_path, input_data):
+    new_onnx_model = convert_float_to_float16_model_path(onnx_model_path, keep_io_types=True)
+    model_name = onnx_model_path.stem
+    quantized_model_file = f'{model_name}_quant_fp16{ONNX_EXT}'
+    onnx.save(new_onnx_model, quantized_model_file)
+    test_onnx_model(quantized_model_file, input_data)
+    logging.info(f"Fp16 quantized model saved to: {quantized_model_file}")
+
+
+def run_quantizations(onnx_model_path, input_data):
+    quantize_model_dynamic(onnx_model_path, QuantType.QUInt8, input_data)
+    # TODO: find why this conversion fails
+    # quantize_model_dynamic(onnx_model_path, QuantType.QInt8, input_data)
+    quantize_model_fp16(onnx_model_path, input_data)
+
+
+def load_craft_model():
     craft = Craft(
         output_dir=None,
         rectify=True,
@@ -78,14 +108,28 @@ def main():
     model = craft.craft_net
 
     model.eval()
+    return model
 
-    batch_size = 1
-    input_data = torch.randn(batch_size, 3, 224, 224, requires_grad=True)
+
+def load_image_to_tensor(image_path):
+    img = Image.open(image_path)
+    transform = transforms.Compose([transforms.Resize(size=(224, 224)),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    tensor = transform(img)
+    tensor = tensor[None, :]
+    tensor.requires_grad = True
+    return tensor
+
+
+def main():
+    model = load_craft_model()
+    input_data = load_image_to_tensor(IMG_PATH)
     torch_out = model(input_data)
     onnx_model_path = Path(ONNX_MODEL_FILE)
-    convert(model, input_data, onnx_model_path)
+    convert_to_onnx(model, input_data, onnx_model_path)
     test_output(torch_out, input_data, onnx_model_path)
-    run_quantizations(onnx_model_path)
+    run_quantizations(onnx_model_path, input_data)
 
 
 if __name__ == '__main__':
